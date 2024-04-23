08b7242d1ed84b3353cec97442ce8e2532324235
Sadiqun Nur Ayan <sadiqunnur.97@gmail.com>
2024-04-24 00:12:12 +0600

    relationships created among the tables

--- accounts/models.py
+++ accounts/models.py
@@ -9,8 +9,14 @@ class Customer(models.Model):
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
-        return f'{self.name} {self.email}'
+        return f'{self.name}'
 
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return f'{self.name}'
 
 class Product(models.Model):
     Category = (
@@ -23,6 +29,12 @@ class Product(models.Model):
     category = models.CharField(max_length=200, null=True, choices=Category)
     description = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
 
 class Order(models.Model):
     STATUS = (
@@ -30,8 +42,12 @@ class Order(models.Model):
         ('Out for delivery', 'Out for delivery'),
         ('Delivery', 'Delivered'),
     )
-    #customer = 
-    #product = 
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
+
+    def __str__(self):
+        return f'{self.customer} {self.product}'
+    
 
