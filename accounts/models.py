60e4f74faadf9d972b008cc3dcb7c1c11704867a
Sadiqun Nur Ayan <sadiqunnur.97@gmail.com>
2024-04-23 10:29:29 +0600

    models created

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return f'{self.name} {self.email}'
+
+
+class Product(models.Model):
+    Category = (
+        ('Indoor', 'Indoor'),
+        ('Outdoor', 'Outdoor'),
+        ('Delivery', 'Delivered'),
+    )
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=Category)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivery', 'Delivered'),
+    )
+    #customer = 
+    #product = 
+    date_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
+
